36988416e86730bf482be16ec1aee26778836a68
yuqing.ji <yuqing.ji@outlook.com>
2020-09-19 12:29:58 +0800

    fix safari mouse press issue

--- rei.js
+++ rei.js
@@ -5,24 +5,25 @@ function setup() {
 
   textFont('Georgia');
 
-  canvasColor = createSelect();
-  canvasColor.position(100, 275);
-  canvasColor.style('width', '150');
-  canvasColor.option('Select Canvas Color');
-  canvasColor.option('Blue');
-  canvasColor.option('White');
+  canvasColorBtn = createSelect().id('canvasColorBtn');
+  canvasColorBtn.position(100, 275);
+  canvasColorBtn.style('width', '150');
+  canvasColorBtn.option('Select Canvas Color');
+  canvasColorBtn.option('Blue');
+  canvasColorBtn.option('White');
 
-  brushBtn = createSelect();
-  brushBtn.position(100, 305);
-  brushBtn.style('width', '150');
-  brushBtn.option('Select Brush Type');
-  brushBtn.option('Circle');
-  brushBtn.option('Cross');
-  brushBtn.option('Line');
-  brushBtn.option('Square');
-  brushBtn.option('Triangle');
+  brushTypeBtn = createSelect().id('brushTypeBtn');
+  //brushTypeBtn = createSelect();
+  brushTypeBtn.position(100, 305);
+  brushTypeBtn.style('width', '150');
+  brushTypeBtn.option('Select Brush Type');
+  brushTypeBtn.option('Circle');
+  brushTypeBtn.option('Cross');
+  brushTypeBtn.option('Line');
+  brushTypeBtn.option('Square');
+  brushTypeBtn.option('Triangle');
 
-  brushColorBtn = createSelect();
+  brushColorBtn = createSelect().id('brushColorBtn');
   brushColorBtn.position(100,335);
   brushColorBtn.style('width', '150');
   brushColorBtn.option('Select Brush Color');
@@ -40,36 +41,54 @@ function setup() {
   screenshotBtn.mousePressed(screenshot);
 
   footer();
+  
 }
+
 
 function draw() {
   header();
   canvas = select('canvas');
-  canvas.elt.style.backgroundColor = canvasColor.value();
-  if (mouseIsPressed == true) {
-    if (brushBtn.value().search('Select') == -1 )  {
-      brush = 'brush' + brushBtn.value();
+  canvas.elt.style.backgroundColor = canvasColorBtn.value();
+  selectBtnSelected('canvasColorBtn');
+  selectBtnSelected('brushTypeBtn');
+  selectBtnSelected('brushColorBtn');
+  if (mouseIsPressed) {
+    if (brushTypeBtn.value().search('Select') == -1)  {
+      brush = 'brush' + brushTypeBtn.value();
       stroke(brushColorBtn.value());
       eval(brush).call();
     }
   }
 }
 
+
+function selectBtnSelected(id) {
+  selectBtn = document.getElementById(id);
+  selectBtn.addEventListener('change', () => {
+    window.dispatchEvent(new Event('mouseup'));
+  });
+}
+
+
 function brushCircle() {
   brushShape('circle');
 }
+
 
 function brushCross() {
   brushShape('cross');
 }
 
+
 function brushLine() {
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
 
+
 function brushSquare() {
   brushShape('square');
 }
+
 
 function brushShape(shape, x = mouseX, y = mouseY, px = pmouseX, py = pmouseY) {
   speed = abs(x - px) + abs(y - py);
@@ -88,29 +107,35 @@ function brushShape(shape, x = mouseX, y = mouseY, px = pmouseX, py = pmouseY) {
   }
 }
 
+
 function brushTriangle() {
   brushShape('triangle');
 }
 
+
 function footer() {
-  copyright = createA("https://gniquyij.github.io/en/about", "© 2018-2020 by YUQING JI", "_blank");
+  copyright = createA('https://gniquyij.github.io/en/about', '© 2018-2020 by YUQING JI', '_blank');
   copyright.style('font-size', '17');
   copyright.position(100, 485);
 }
 
+
 function header() {
   image(logo, 100, 120, logo.width/2, logo.height/2);
 }
+
 
 function reset() {
   clear();
   setup();
 }
 
+
 function screenshot() {
   t = time();
   saveCanvas('rei-' + t, 'png');
 }
+
 
 function time() {
   return year() + '-' + month() + '-' + day();
